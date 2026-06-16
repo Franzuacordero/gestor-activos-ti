6c8ea99a202d034d42b2dd0b65ab84f13ca6ba2e
emmcordero <102300703+Franzuacordero@users.noreply.github.com>
2026-06-16 02:44:41 -0400

    Fix database connection arguments

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://gestor_activos_db_user:m2qL3auEBBldieBR8M4pNB05cbXuqgCW@dpg-d8o4absm0tmc73aka2l0-a/gestor_activos_db"
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
