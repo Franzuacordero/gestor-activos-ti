54ef99667870909107ab2ef9badd05bd6f0a2a7e
emmcordero <102300703+Franzuacordero@users.noreply.github.com>
2026-06-16 02:42:21 -0400

    Fix database URL in database.py

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://gestor_activos_db_user:m2qL3auEBBldieBR8M4pNB05cbXuqgCW@dpg-d8o4absm0tmc73aka2l0-a/gestor_activ
-
+SQLALCHEMY_DATABASE_URL = "postgresql://gestor_activos_db_user:m2qL3auEBBldieBR8M4pNB05cbXuqgCW@dpg-d8o4absm0tmc73aka2l0-a/gestor_activos_db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
